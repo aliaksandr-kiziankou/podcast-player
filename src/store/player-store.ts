2a600dda393c599aeb1a7c08a7d69b6a17d21dfb
Alex Kizenkov <aliaksandr.kiziankou@gmail.com>
2026-07-09 17:49:12 +0500

    feat: add player store

---
+++ src/store/player-store.ts
@@ -0,0 +1,38 @@
+export type Episode = {
+    id: string;
+    title: string;
+    URL: string;
+};
+
+let currentEpisode: Episode | null = null;
+let isPlaying = false;
+
+type Listener = () => void;
+const LISTENERS: Listener[] = [];
+
+export function updChanger(listener: Listener) : void {
+    LISTENERS.push(listener);
+};
+
+function notifyChanges(): void {
+    LISTENERS.forEach((listener) => listener());
+};
+
+export function setEpisode(episode: Episode): void {
+    currentEpisode = episode;
+    isPlaying = true;
+    notifyChanges();
+};
+
+export function togglePlay(): void {
+    isPlaying = !isPlaying;
+    notifyChanges();
+};
+
+export function getCurretnEpisode(): Episode | null {
+    return currentEpisode;
+};
+
+export function getIsPlaying(): boolean {
+    return isPlaying;
+};
